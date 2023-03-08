19c5da6311fbbf1744e8d90b58b851fd4682f48d
MRMilton912 <multimillionairemilton@gmail.com>
2023-03-08 07:04:23 -0500

    For loop

--- pokestyle/script.js
+++ pokestyle/script.js
@@ -1,5 +1,16 @@
 let pokemonList = [
-  {name: "Bulbasaur", height: 7, type: ["grass", "poison"]},
-  {name: "Squirtle", height: 5, type: ["water"]},
-  {name: "Pikachu", height: 4, type: ["electric"]}
-]
+  {
+    name: "Bulbasaur",
+    height: 7,
+    type: ["grass", "poison"]},
+  {
+    name: "Squirtle",
+    height: 5,
+    type: ["water"]},
+  {
+    name: "Pikachu",
+    height: 4,
+    type: ["electric"]}
+];
+
+for (let i=0; i <pokemonList.length; i++);
