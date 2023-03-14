00fbcf0011bc0bd4cee2c1abbb4a0d0ff7f0cfbb
MRMilton912 <multimillionairemilton@gmail.com>
2023-03-14 13:40:06 -0400

    Update script.js

--- pokescript/script.js
+++ pokescript/script.js
@@ -1,66 +1,46 @@
 let pokeDex = (function () {
-  let pokemonList = [
-    {
-      name: "Bulbasaur",
-      height: 0.7,
-      type: ["grass", "poison"]},
-    {
-      name: "Squirtle",
-      height: 0.5,
-      type: ["water"]},
-    {
-      name: "Pikachu",
-      height: 0.4,
-      type: ["electric"]},
-    {
-      name: "Charizard",
-      height: 1.7,
-      type: ["fire"]}
-  ];
+  let pokemonList = [];
+  let pokeweb = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-  //pokemonList.forEach(function (item){
-  //document.write (item.name +  "(height: "+item.height+")" + "(type: "+item.type+")" )});
-  //pokemonList.forEach(function(pokemon)
-  //console.log(Pokemon.name + "(height: "+Pokemon.height+")" + "(type: "+Pokemon.type+")" )
+  function add(pokemon){ 
+    if (
+      typeof pokemon === 'object' &&
+      "name" in pokemon &&
+      "detailsUrl" in pokemon) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log('Invalid Pokémon');
+    }}
 
-  function getAll (){
-    return pokemonList;
-  }
-  function add (pokemon) {
-    pokemonList.push(pokemon)
-  }
-
-  function addListItem(pokemon){
-    let pokemonList = document.querySelector(".pokemon-list");
-    let listpokemon = document.createElement("li");
-    let button = document.createElement("button");
-    button.innerText = pokemon.name;
-    button.classList.add("poke-button");
-    button.addEventListener('click', function(event) {console.log(event)});
-    listpokemon.appendChild(button);
-    pokemonList.appendChild(listpokemon);
+  function loadList(){
+    return fetch(pokeweb).then(function (response) {
+      return response.json()
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
   }
 
   return {
     getAll: getAll,
     add: add,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList
   };
 
-  })()
+})();
 
   console.log(pokeDex.getAll());
-  pokeDex.add({ 
-    name: "Onix",
-    height: 8.8,
-    type: ["rock"]
+
+  pokeDex.loadList().then(function() {
+    pokeDex.getAll().forEach(function (pokemon) {
+      pokeDex.addListItem(pokemon);
+    });
   });
-
-  pokeDex.getAll().forEach(function(pokemon) {
-    pokeDex.addListItem(pokemon)
-
-  });
-
-  showDetails (pokemon); {
-    console.log (pokemon)
-  };
