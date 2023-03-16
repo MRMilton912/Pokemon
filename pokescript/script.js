a1b30020c480cc5eed7938af58b25233fab0c4f7
MRMilton912 <multimillionairemilton@gmail.com>
2023-03-16 15:18:54 -0400

    Div

--- pokescript/script.js
+++ pokescript/script.js
@@ -63,52 +63,52 @@ let pokeDex = (function () {
 
     });
 
-      let modalContainer = document.querySelector
-      ('#pokemon-list');
-      function showModal(title, text) {
-        modalContainer.innerHTML = '';
-        let modal = document.createElement('div');
-        modal.classList.add('modal')
 
+    function showModal(title, text) {
+      let modalContainer = document.querySelector('#modal-container');
+      modalContainer.classList.add('is-visible');
+
+    document.querySelector('#show-modal').addEventListener('click', () => {
+      showModal();
+    
+      function showModal(title, text) {
+        let modalContainer = document.querySelector('#modal-container');
+      
+        // Clear all existing modal content
+        modalContainer.innerHTML = '';
+      
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+      
+        // Add the new modal content
         let closeButtonElement = document.createElement('button');
         closeButtonElement.classList.add('modal-close');
         closeButtonElement.innerText = 'Close';
-        closeButtonElement.addEventListener('click', hideModal);
-
+      
         let titleElement = document.createElement('h1');
         titleElement.innerText = title;
-
+      
         let contentElement = document.createElement('p');
         contentElement.innerText = text;
-
+      
         modal.appendChild(closeButtonElement);
         modal.appendChild(titleElement);
         modal.appendChild(contentElement);
         modalContainer.appendChild(modal);
+      
+      
+      
         modalContainer.classList.add('is-visible');
+      }
+      
+      document.querySelector('#show-modal').addEventListener('click', () => {
+        showModal('Modal title', 'This is the modal content!');
 
         function hideModal() {
+          let modalContainer = document.querySelector('#modal-container');
           modalContainer.classList.remove('is-visible');
-        }
-
-        window.addEventListener('keydown', (e) => {
-          if (e.key === 'Escape' &&
-          modalContainer.classList.contains('is-visible')) {
-            hideModal();
-          }
-        });
-        modalContainer.addEventListener('click', (e) => {
-          let target = e.target;
-          if (target === modalContainer) {
-            hideModal();
-          }
-        });
-
-        document.querySelector('#pokemon-list'),addEventListener('click', () => {
-          showModal(pokemon.name, pokemon.height, pokemon.type)
-        });
-      }
-    };
+      };
+    });
 
   return {
     add: add,
@@ -118,9 +118,9 @@ let pokeDex = (function () {
     loadDetails: loadDetails,
     showDetails: showDetails
   };
-})();
+
   pokeDex.loadList().then(function() {
     pokeDex.getAll().forEach(function (pokemon) {
       pokeDex.addListItem(pokemon);
     });
-  })
+  })})}}})
