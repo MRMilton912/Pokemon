d430320ab9f3c83074f770bd2afbecffdec3e25a
MRMilton912 <multimillionairemilton@gmail.com>
2023-03-17 10:38:00 -0400

    Modal

---
+++ pokescript/poke.js
@@ -0,0 +1,4 @@
+let pokeDex = (function () {
+  let pokemonList = [];
+  let pokeweb = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+})(); 
--- pokescript/script.js
+++ pokescript/script.js
@@ -63,60 +63,71 @@ let pokeDex = (function () {
     });
   }
 
-    function showModal(title, text) {
+  function showModal(title, text) {/*broken*/
+    let modalContainer = document.querySelector('#modal-container');
+  
+    // Clear all existing modal content
+    modalContainer.innerHTML = '';
+  
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+  
+    // Add the new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+    window.addEventListener('keydown', (e) => {
       let modalContainer = document.querySelector('#modal-container');
-      modalContainer.classList.add('is-visible');
-      document.querySelector('#show-modal').addEventListener('click', () => {
-      showModal();
-    
-        // Clear all existing modal content
-        modalContainer.innerHTML = '';
-      
-        let modal = document.createElement('div');
-        modal.classList.add('modal');
-      
-        // Add the new modal content
-        let closeButtonElement = document.createElement('button');
-        closeButtonElement.classList.add('modal-close');
-        closeButtonElement.innerText = 'Close';
-      
-        let titleElement = document.createElement('h1');
-        titleElement.innerText = title;
-      
-        let contentElement = document.createElement('p');
-        contentElement.innerText = text;
-      
-        modal.appendChild(closeButtonElement);
-        modal.appendChild(titleElement);
-        modal.appendChild(contentElement);
-        modalContainer.appendChild(modal);
-      
-      
-      
-        modalContainer.classList.add('is-visible');
-      });
-      
-      document.querySelector('#show-modal').addEventListener('click', () => {
-        showModal('Modal title', 'This is the modal content!');
+      if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+        hideModal();  
+      }
 
-//        function hideModal() {
-//          let modalContainer = document.querySelector('#modal-container');
-//          modalContainer.classList.remove('is-visible');
-//      };
-    });
-  }
+  
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+  
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+  
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
 
-    return {
-      add: add,
-      getAll: getAll,
-      addListItem: addListItem,
-      loadList: loadList,
-      showDetails: showDetails
-    };
-  })()
-
-  pokeDex.loadList().then(function() {
-    pokeDex.getAll().forEach(function (pokemon) {
-      pokeDex.addListItem(pokemon);
-    });
+    modalContainer.classList.add('is-visible');
   });
+  
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('Modal title', 'This is the modal content!');
+
+    function hideModal() {
+      let modalContainer = document.querySelector('#modal-container');
+      modalContainer.classList.remove('is-visible');
+    }
+
+        modalContainer.addEventListener('click', (e) => {
+          // Since this is also triggered when clicking INSIDE the modal
+          // We only want to close if the user clicks directly on the overlay
+          let target = e.target;
+          if (target === modalContainer) {
+            hideModal();
+          }
+        });
+  })();
+
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    showDetails: showDetails
+  };
+})
+
+pokeDex.loadList().then(function() {
+  pokeDex.getAll().forEach(function (pokemon) {
+    pokeDex.addListItem(pokemon);
+  });
+})
